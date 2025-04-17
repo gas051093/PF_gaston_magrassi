cae66813717cb85423dd8058025d6f37471b6b31
gaston-magrassi <gas051093@hotmail.com.ar>
2025-04-17 00:36:23 -0300

    se agrego la seccion lista de equipos y modificacion de sass y bem

--- js/scrips.js
+++ js/scrips.js
@@ -2,30 +2,33 @@
 // let pass = "admin";
 // const equipos = [];
 // //login del usuario con validacion//
-// function login() { 
+// function login() {
 //     for (i = 5; i > 0; i--) {
 //         let usuario = prompt('Ingrese el nombre de usuario (admin)');
 //         let contraseña = prompt('Ingrese la contraseña (admin)');
 //         if (usuario === user && contraseña === pass) {
 //             return true;
-//         } else { 
+
+//         } else {
 //             alert(`Error! Intente de nuevo, le quedan: ${i} intentos`)
 //         }
 //     }
 //     return false
 // }
 // //funcion de seleccion de agregar elementos al array//
-// function agregar() { 
+
+// function agregar() {
 //     let input = prompt(`Ingrese el nombre nombre del equipo a reparar`);
 //     if (input) {
 //         equipos.push(input);
 //         alert(`Se agrego el equipo: ${input}`)
-//     } else { 
+
+//     } else {
 //         alert(`Error no se ingreso ningun equipo`)
 //     }
 // }
 // //funcion de ver la lista de qeuipos ingresados/
-// function listar() { 
+// function listar() {
 //     if (equipos.length > 0) {
 //       alert(equipos.join(`\n`));
 //     } else {
@@ -33,19 +36,19 @@
 //     }
 // }
 // //funcion de eliminar ultimo elemento ingresado en el array//
-// function eliminar() { 
+// function eliminar() {
 //     if (equipos.length > 0) {
 //         let equipo_eliminado = equipos.pop();
 //         alert(`el equipo eliminado es:  ${equipo_eliminado}`);
-//     } else { 
+//     } else {
 //         alert(`La lista esta vacia`)
 //     }
 // }
 // //funcion de seleccion de menu//
-// function menu() { 
+// function menu() {
 //     let select_menu = parseInt(prompt(`Bienvenido a la App de Csistemas \n Ingrese la opcion que desee realizar \n 1) Cargar equipo \n 2) ver lista de equipo \n 3) Borrar ultimo equipo \n 4) salir del sistema `))
-//     while (select_menu !== 4) { 
-//         switch (select_menu) { 
+//     while (select_menu !== 4) {
+//         switch (select_menu) {
 //             case 1:
 //                 agregar();
 //                 break;
