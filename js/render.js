3dbb2f1237816a5aa45bada0eb57213222fd2488
gaston-magrassi <gas051093@hotmail.com.ar>
2025-05-14 00:22:01 -0300

    modificacion de js-render

--- js/render.js
+++ js/render.js
@@ -1,11 +1,16 @@
-function select_menu () { 
+const form_alta = document.getElementById("altaOT");
+function show_alta() { 
+    form_alta.classList.remove("d-none");
+}
+
+function select_menu() { 
     const btn_select = document.querySelectorAll(".sidebar__item");
     btn_select.forEach(btn => { 
         btn.addEventListener("click", (e) => { 
             const btn_id = e.currentTarget.id;
             switch (btn_id) { 
                 case "btn_new":
-                    console.log(1);
+                    show_alta();
                     break
                 case "btn_find": 
                     console.log(2)
